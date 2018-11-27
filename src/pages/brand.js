c47d41e4dbd36d6c52335f05add62bbe980e8762
Jason L Perry <jason@suncoast.io>
2018-11-27 10:32:04 -0500

    Update
    A lazy commit.

---
+++ src/pages/brand.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const BrandPage = () => (
+  <Layout>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Brand Identity</h1>
+          <h2 class="subtitle">
+            Guidelines for using the Suncoast Developers Guild Brand
+          </h2>
+        </div>
+      </div>
+    </section>
+  </Layout>
+)
+
+export default BrandPage
