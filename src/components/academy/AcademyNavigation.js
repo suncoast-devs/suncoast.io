c6a77f1b828751a531e96154d5643a686bf0dcbc
Jason L Perry <jason@suncoast.io>
2018-12-23 11:31:26 -0500

    Update
    A lazy commit.

--- src/components/academy/AcademyNavigation.js
+++ src/components/academy/AcademyNavigation.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import cx from 'classnames'
+import { Location } from '@reach/router'
 import { resolve } from '@reach/router/lib/utils'
 import Container from '../Container'
 import Icon from '../Icon'
 
 const NavItem = ({ to, icon, children }) => (
-  <div
-    className={cx('column has-text-centered', {
-      'is-active': window.location.pathname.startsWith(resolve(to)),
-    })}
-  >
-    <div>
-      <Link activeClassName="is-active" to={to}>
-        <Icon i={icon} />
-        <span className="heading">{children}</span>
-      </Link>
-    </div>
-  </div>
+  <Location>
+    {({ location }) => (
+      <div
+        className={cx('column has-text-centered', {
+          'is-active': location.pathname.startsWith(resolve(to)),
+        })}
+      >
+        <div>
+          <Link activeClassName="is-active" to={to}>
+            <Icon i={icon} />
+            <span className="heading">{children}</span>
+          </Link>
+        </div>
+      </div>
+    )}
+  </Location>
 )
 
 const ProgramNavigation = () => (
