f7c9f95a0a97459d421528d6976a133edbe87524
Jason L Perry <jason@suncoast.io>
2018-12-31 15:16:26 -0500

    Update
    A lazy commit.

--- src/components/MissionStatement.js
+++ src/components/MissionStatement.js
@@ -1,29 +1,48 @@
 import React from 'react'
-import temp from '../images/temp.svg'
+import { StaticQuery, graphql } from 'gatsby'
+import Image from 'gatsby-image'
 
 const MissionStatement = () => (
-  <section className="mission-statement section">
-    <div className="container">
-      <div className="columns">
-        <div className="column has-text-centered is-narrow">
-          <figure>
-            <img src={temp} alt="Placeholder Illustration" width="200" />
-          </figure>
+  <StaticQuery
+    query={graphql`
+      query MissionStatementQuery {
+        org: file(relativePath: { eq: "organization.jpg" }) {
+          childImageSharp {
+            fixed(width: 200) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `}
+    render={({ org }) => (
+      <section className="mission-statement section">
+        <div className="container">
+          <div className="columns is-vcentered">
+            <div className="column has-text-centered is-narrow">
+              <Image
+                Tag="figure"
+                className="image"
+                fixed={org.childImageSharp.fixed}
+                alt="A portrait of our team"
+              />
+            </div>
+            <div className="mission column">
+              <h2 className="title">Our Organization</h2>
+              <p>
+                Our mission is to provide those seeking an education in software
+                development with the technical and soft skills they need to
+                pursue rewarding careers as programmers. We also seek to promote
+                a sense of community among local technology organizations,
+                supporting members in a way that strengthens our community and
+                promotes the common good.
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="mission column">
-          <h2 className="title">Our Organization</h2>
-          <p>
-            Our mission is to provide those seeking an education in software
-            development with the technical and soft skills they need to pursue
-            rewarding careers as programmers. We also seek to promote a sense of
-            community among local technology organizations, supporting members
-            in a way that strengthens our community and promotes the common
-            good.
-          </p>
-        </div>
-      </div>
-    </div>
-  </section>
+      </section>
+    )}
+  />
 )
 
 export default MissionStatement
